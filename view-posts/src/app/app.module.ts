cacb5e90c330997532f4b9e8c44eda828cccf4f8
myxac <62265296+myxac@users.noreply.github.com>
2021-03-10 23:01:53 +0200

    init app && create pages/services/components

---
+++ view-posts/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './pages/list/list.component';
+import { ViewComponent } from './pages/view/view.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoaderComponent } from './components/loader/loader.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    ViewComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
